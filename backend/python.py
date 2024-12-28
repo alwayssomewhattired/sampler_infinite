03352269e46a1a364087989b759db90c463e87ee
Zachary Malinka <zacharymalinka@gmail.com>
2024-12-27 21:26:17 -0500

    made 'downloader' function that can download mp3 from internet archive

--- backend/python.py
+++ backend/python.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from internetarchive import download
 
 app = Flask(__name__)
 CORS(app)
@@ -8,7 +9,13 @@ CORS(app)
 def receive_data():
     data = request.get_json() 
     print(data)  # Print the received data
-    return jsonify({'message': 'Data received successfully!'})
+    # return jsonify({'message': 'Data received successfully!'})
+    
+def downloader():
+    return print(download(identifier="Ex_Military-9086", destdir="C:\\Users\\zacha\\Desktop\\empty"))
+
+downloader()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend/src/components/Api.jsx
+++ frontend/src/components/Api.jsx
@@ -1,16 +1,13 @@
-
-
 export default function Api(gDirectory) {
-
-    fetch("http://127.0.0.1:5000/api/endpoint", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-        },
-        body: JSON.stringify(gDirectory),
-      })
-        .then((response) => response.json())
-        .then((data) => console.log(data))
-        .catch((error) => console.error("Error:", error));
+  fetch("http://127.0.0.1:5000/api/endpoint", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify(gDirectory),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error("Error:", error));
 }
