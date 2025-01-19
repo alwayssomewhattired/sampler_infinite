3f8ba4cb2f1a9ffafeecbd2c7da0e33a376fbfbe
Zachary Malinka <zacharymalinka@gmail.com>
2025-01-18 23:07:50 -0500

    Audio analyzer node is set up and can analyze frequencies. SUCCESS! Now we need to find when the prominent frequency happens...

--- frontend/src/components/AudioEditor.jsx
+++ frontend/src/components/AudioEditor.jsx
@@ -3,8 +3,17 @@
 // import { useRef } from "react";
 
 export default function AudioEditor() {
-
   const ctx = new AudioContext();
+  const analyser = ctx.createAnalyser();
+  analyser.fftSize = 512;
+  const bufferLength = analyser.frequencyBinCount;
+
+  const dataArray = new Uint8Array(bufferLength);
+
+  const canvas = document.getElementById("canvas");
+  const canvasCtx = canvas.getContext("2d");
+  canvasCtx.clearRect(0, 0, 150, 100);
+
   let audio;
 
   fetch(
@@ -16,7 +25,7 @@ export default function AudioEditor() {
     }
   )
     .then((data) => data.arrayBuffer())
-    .then((arrayBufer) => ctx.decodeAudioData(arrayBufer))
+    .then((arrayBuffer) => ctx.decodeAudioData(arrayBuffer))
     .then((decodedAudio) => {
       audio = decodedAudio;
     });
@@ -24,31 +33,58 @@ export default function AudioEditor() {
   function playBack() {
     const playSound = ctx.createBufferSource();
     playSound.buffer = audio;
+    playSound.connect(analyser);
     playSound.connect(ctx.destination);
     playSound.start(ctx.currentTime);
   }
 
   window.addEventListener("mousedown", playBack);
 
-  const playButton = document.querySelector("button");
+  function draw() {
+    const drawVisual = requestAnimationFrame(draw);
+    analyser.getByteFrequencyData(dataArray);
 
-  playButton.addEventListener(
-    "click",
-    () => {
-      if (playBack.state === "suspended") {
-        playBack.resume();
-      }
-      if (playButton.dataset.playing === "false") {
-        playBack.play();
-        playButton.dataset.playing = "true";
-      } else if (playButton.dataset.playing === "true") {
-        playBack.pause();
-        playButton.dataset.playing = "false";
-      }
-    },
-    false
-  );
-  //              NO IDEA HOW TO WORK THIS
+    //                                     WRITE CONDITIONAL TO FIND PROMINENT FREQUENCY AND CONSOLE.LOG WHEN IT IS FOUND
+    // 1. add 20 to every element in dataArray except index of what you are trying to find
+    // 2. if ((dataArray[0] - 20) > Math.max(...dataArray)){
+    //        console.log(good)}
+
+    //     FAILED: CANNOT ISOLATE SPECIFIC INDEX BECAUSE DATA IS FLUID AND UINT ARRAYS ARE IMMUTABLE
+
+    //                                      THAT IS A LITTLE TRICKY... TRY 'EVERY' METHOD INSTEAD.
+    //                                      YOU WILL NEED TO PUSH EVERY ELEMENT EXCEPT THE ELEMENT OF INDEX YOU ARE TESTING INTO A NEW ARRAY...
+    //                                      THIS IS BECAUSE IF YOU REMOVE AN INDEX, THE DATA WILL SPILL OVER INTO THE REST OF THE ARRAY...
+    //                                      WHICH MEANS THE DATA YOU REMOVED IS STILL THERE.
+    //                                      IF YOU PUSH ALL DATA THAT COMES BEFORE AND AFTER THE ELEMENT YOU ARE TESTING INTO A NEW ARRAY...
+    //                                      AND THEN TEST THE NEW ARRAY AGAINST THE SHRUNKEN ELEMENT...
+    //                                      YOU WILL HAVE A PRETTY GOOD IDEA OF WHEN THAT FREQUENCY IS PROMINENT
+    //1. if((dataArray[0] - 20) > )
+
+    // console.log(dataArray[0] - 20)
+
+    const WIDTH = 150;
+    const HEIGHT = 100;
+
+    canvasCtx.fillStyle = "rgb(0 0 0)";
+    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+    const barWidth = (WIDTH / bufferLength) * 2.5;
+    let barHeight;
+    let x = 0;
+
+    for (let i = 0; i < bufferLength; i++) {
+      barHeight = dataArray[i] / 2;
+
+      canvasCtx.fillStyle = `rgb(${barHeight + 100} 50 50)`;
+      canvasCtx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight);
+
+      x += barWidth + 1;
+    }
+  }
+
+  draw();
+
+  //                                                             NO IDEA HOW TO WORK THIS
 
   // async function getData() {
   //   const url =
